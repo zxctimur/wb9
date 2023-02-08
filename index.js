0ec956ea2362af93637016e01f0cc05c1a47aa36
zxctimur <gerasenkotimur207@gmail.com>
2023-02-08 20:07:14 +0300

    str,arr

---
+++ index.js
@@ -0,0 +1,43 @@
+// let str1 = `Hello`
+// let str2 = `world!`
+// console.log(str1.concat(', ',str2))
+
+// let a = 10
+// let b = 2
+// let c = 5
+// let sum = a+b+c
+// console.log(sum)
+
+// let a = 17
+// let b = 10
+// let c = a-b
+// console.log(c)
+// let d = 7
+// let result = c+d
+// console.log(result)
+
+// let str = `I learn javascript!`
+// console.log(str.replaceAll(' ','_'))
+
+// let str = `I learn javascript!`
+// console.log(str.length)
+
+// let arr = [1,2,3,4]
+// result = (arr[0]*arr[1])+(arr[2]*arr[3])
+// console.log(result)
+
+// let str = "я учусь в it-academy"
+// let arr = str.toLowerCase().split('')
+// arr[0] = arr[0].toLowerCase()
+// arr[2] = arr[2].toUpperCase()
+// arr[4] = arr[4].toLowerCase()
+// arr[6] = arr[6].toUpperCase()
+// arr[8] = arr[8].toLowerCase()
+// arr[10] = arr[10].toUpperCase()
+// arr[12] = arr[12].toLowerCase()
+// arr[14] = arr[14].toUpperCase()
+// arr[16] = arr[16].toLowerCase()
+// arr[18] = arr[18].toUpperCase()
+
+// let str1=arr.join('')
+// console.log(str1)
